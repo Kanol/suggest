724dad77f70512920a91cdc4c7efaff2da4a02f5
andre <andrey.karmanov.2011@yandex.ru>
2019-10-04 10:04:46 +0500

    Initial commit

---
+++ tests.py
@@ -0,0 +1,24 @@
+import unittest
+from suggest import Suggester
+
+
+class SuggesterTest (unittest.TestCase):
+    def setUp(self):
+        self.suggester = Suggester ("ruwords.txt")
+
+    def test_get_find_anything(self):
+        result = self.suggester.get("аббат")
+        self.assertGreater(len(result), 0)
+
+    def test_get_find_too_more(self):
+        result = self.suggester.get("a")
+        self.assertEqual(len(result), 10)
+
+    def test_get_empty_word(self):
+        result = self.suggester.get("")
+        print(result)
+        self.assertEqual(len(result), 0)
+
+
+if __name__ == '__main__':
+    unittest.main ()
